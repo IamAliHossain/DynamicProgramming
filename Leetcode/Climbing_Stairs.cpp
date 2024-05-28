686d7a58a0f3ce3259335faefe5deacd485ebb5a
Ali Hossain <alihossain.cse.pstu@gamail.com>
2024-05-28 23:51:54 +0600

    New Problem solved

---
+++ Leetcode/Climbing_Stairs.cpp
@@ -0,0 +1,81 @@
+/*
+    Problem Link : https://leetcode.com/problems/climbing-stairs 
+    Author : Ali Hossain
+    Date : 28-05-2024
+
+    70. Climbing Stairs
+    Difficulty : Easy
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+/*****************Top-Down Approach (Memoization)************************/
+
+class Solution {
+public:
+    
+    int climbingStairs(int n, int dp[]){
+        if(n < 0){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
+        if(dp[n] != -1)return dp[n];
+        int step1 = 0, step2= 0;
+        step1 += climbingStairs(n - 1, dp);
+        step2 += climbingStairs(n - 2, dp);
+        
+        return dp[n] = step1 + step2;
+    }
+    
+    int climbStairs(int n) {
+        int dp[n+1];
+        memset(dp, -1, sizeof(dp));
+        return climbingStairs(n, dp);
+    }
+};
+
+/*****************Bottom-Up Approach (Tabulatioon)************************/
+/* Basically This was a fibonacci technique */
+class Solution {
+public:
+    
+    int climbStairs(int n) {
+        int dp[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+};
